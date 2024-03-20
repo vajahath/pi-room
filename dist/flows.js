c4c62c749151034931a934c9fbf6aa9eae8ff4b6
Vajahath Ahmed <vajuoff.1@gmail.com>
2024-03-20 12:03:53 +0530

    testing

--- dist/flows.js
+++ dist/flows.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.buttonBasedLedBlink = exports.ledBlink = void 0;
 const johnny_five_1 = __importDefault(require("johnny-five"));
 const io_rx_1 = require("./io-rx");
-const rxjs_1 = require("rxjs");
 const { Led } = johnny_five_1.default;
 async function ledBlink(pin, ms) {
     const led = new Led(pin);
@@ -16,19 +15,22 @@ async function ledBlink(pin, ms) {
 exports.ledBlink = ledBlink;
 async function buttonBasedLedBlink(buttonPin, ledPin) {
     const button = new io_rx_1.Button(buttonPin);
-    button
-        .onKeepPressed()
-        .pipe((0, rxjs_1.filter)((ms) => ms >= 2000), (0, rxjs_1.scan)((state) => !state, false), (0, rxjs_1.switchMap)((on) => {
-        if (on)
-            return button.onClick().pipe((0, rxjs_1.scan)((ms) => ms + 100, 100));
-        return (0, rxjs_1.of)(null);
-    }))
-        .subscribe(async (ms) => {
-        const led = new Led(ledPin);
-        if (ms)
-            return led.blink(ms);
-        led.off();
-    });
+    console.log(button);
+    // button
+    //   .onKeepPressed()
+    //   .pipe(
+    //     filter((ms) => ms >= 2000),
+    //     scan((state) => !state, false),
+    //     switchMap((on) => {
+    //       if (on) return button.onClick().pipe(scan((ms) => ms + 100, 100));
+    //       return of(null);
+    //     })
+    //   )
+    //   .subscribe(async (ms) => {
+    //     const led = new Led(ledPin);
+    //     if (ms) return led.blink(ms);
+    //     led.off();
+    //   });
 }
 exports.buttonBasedLedBlink = buttonBasedLedBlink;
 //# sourceMappingURL=flows.js.map
--- src/flows.ts
+++ src/flows.ts
@@ -15,20 +15,21 @@ export async function ledBlink(pin: string, ms: number) {
 export async function buttonBasedLedBlink(buttonPin: string, ledPin: string) {
   const button = new Button(buttonPin);
 
-  button
-    .onKeepPressed()
-    .pipe(
-      filter((ms) => ms >= 2000),
-      scan((state) => !state, false),
-      switchMap((on) => {
-        if (on) return button.onClick().pipe(scan((ms) => ms + 100, 100));
-        return of(null);
-      })
-    )
-    .subscribe(async (ms) => {
-      const led = new Led(ledPin);
-      if (ms) return led.blink(ms);
+  console.log(button);
+  // button
+  //   .onKeepPressed()
+  //   .pipe(
+  //     filter((ms) => ms >= 2000),
+  //     scan((state) => !state, false),
+  //     switchMap((on) => {
+  //       if (on) return button.onClick().pipe(scan((ms) => ms + 100, 100));
+  //       return of(null);
+  //     })
+  //   )
+  //   .subscribe(async (ms) => {
+  //     const led = new Led(ledPin);
+  //     if (ms) return led.blink(ms);
 
-      led.off();
-    });
+  //     led.off();
+  //   });
 }
